4272e6b3f286520b012263e1d7c82a4149960574
Mahmood Dehghan <mahmood.dehghan@gmail.com>
2013-01-02 08:14:19 +0330

    bug fix

--- Lucene.Net.Analysis.Fa/PersianNormalizationFilter.cs
+++ Lucene.Net.Analysis.Fa/PersianNormalizationFilter.cs
@@ -5,13 +5,13 @@ namespace Lucene.Net.Analysis.Fa
     public sealed class PersianNormalizationFilter : TokenFilter
     {
         private readonly PersianNormalizer _normalizer;
-        private readonly TermAttribute _termAtt;
+        private readonly ITermAttribute _termAtt;
 
         public PersianNormalizationFilter(TokenStream input)
             : base(input)
         {
             _normalizer = new PersianNormalizer();
-            _termAtt = (TermAttribute)AddAttribute<TermAttribute>();
+            _termAtt = AddAttribute<ITermAttribute>();
         }
 
         public override bool IncrementToken()
